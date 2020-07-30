d9953d30dc3debae4e7a872842e29ed7c9c9c3e9
azazulh92 <azazul.cse@gmail.com>
2020-07-30 13:47:46 +0530

    Replace a character of a string

--- practice.go
+++ practice.go
@@ -2,18 +2,10 @@ package main
 
 import "fmt"
 
-var a = 50
-var b = 954
+// replace a character of a string
 
 func main() {
-	var Add int = a + b
-	fmt.Printf("Addition of two numbers is: %d\n", Add)
-	var Substract int = a - b
-	fmt.Printf("Substraction of two numbers is: %d\n", Substract)
-	var Multiply = a * b
-	fmt.Printf("Multipicatiob of two numbers is:%d\n", Multiply)
-	var div = b / a
-	fmt.Printf("Division:%d\n", div)
-	var remainder = b % a
-	fmt.Printf("remainder is:2%d\n", remainder)
+	var s string = "Azazul"
+	repl := s[:1] + "j" + s[2:]
+	fmt.Printf("In string Azazul , z replace as j:=%s", repl)
 }
